e862b82ee4fef250d5ed56701e4f904f971969a8
Emilia <m.emilia.guadalupe@gmail.com>
2021-03-27 18:16:51 -0300

    Making the Tetris game :)

---
+++ src/Components/Display.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function Display({gameOver, text}){
+    return(
+        <>
+            <div>
+                {text}
+            </div>
+        </>
+    )
+}
+
+export default Display;
