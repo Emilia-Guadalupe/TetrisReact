c126b4e906ed0a33ef7d585130520ee8fe68801f
Emilia <m.emilia.guadalupe@gmail.com>
2021-03-29 10:06:06 -0300

    Making the game work

--- src/hooks/usePlayer.js
+++ src/hooks/usePlayer.js
@@ -18,16 +18,13 @@ export const usePlayer = () => {
         }))
     }
 
-    const resetPlayer = useCallback(
-        () => {
-            setPlayer({
-                pos: {x: STAGE_WIDTH / 2 - 2, y: 0},
-                tetromino: randomTetromino().shape,
-                collided: false
-            })
-        },
-        [],
-    )
+    const resetPlayer = useCallback(() => {
+        setPlayer({
+        pos: { x: STAGE_WIDTH / 2 - 2, y: 0 },
+        tetromino: randomTetromino().shape,
+        collided: false,
+        })
+    }, [])
 
     return[player, updatePlayerPos, resetPlayer];
 }
--- src/hooks/useStage.js
+++ src/hooks/useStage.js
@@ -6,28 +6,32 @@ export const useStage = (player, resetPlayer) => {
 
     useEffect(() => {
         const updateStage = prevStage => {
-            // First flush the stage
-            const newStage = prevStage.map(row => 
-                row.map(cell => (cell[1] === 'clear' ? [0, 'clear'] : cell)));
-
-            //Then draw the Tetromino
-            player.tetromino.forEach((row, y) => {
-                row.forEach((value, x) => {
-                    if(value !== 0) {
-                        newStage[y + player.pos.y][x + player.pos.x] = [
-                            value,
-                            `${player.collided ? 'merged' : 'clear'}`,
-                        ];
-                    }
-                });
+          // First flush the stage
+        const newStage = prevStage.map(row =>
+            row.map(cell => (cell[1] === 'clear' ? [0, 'clear'] : cell)),
+        );
+    
+          // Then draw the tetromino
+        player.tetromino.forEach((row, y) => {
+            row.forEach((value, x) => {
+            if (value !== 0) {
+                newStage[y + player.pos.y][x + player.pos.x] = [
+                value,
+                `${player.collided ? 'merged' : 'clear'}`,
+                ];
+            }
             });
-
-            return newStage;
+        });
+        // Then check if we collided
+        if(player.collided) {
+            resetPlayer();
+        }
+    
+        return newStage;
         };
-
-        setStage(prev => updateStage(prev))
-
-    }, [player])
-
+    
+        setStage(prev => updateStage(prev));
+    }, [player, resetPlayer]);
+    
     return [stage, setStage];
-}
+};
